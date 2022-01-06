3d7e938c039d1adf082f5e71d69dc2352b52219b
Tenyo Grozev <tenyo.grozev@yale.edu>
2022-01-05 21:26:42 -0500

    switch to Vue3 for frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,10 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-Vue.config.productionTip = false
-
-const client = axios.create({
+const axiosClient = axios.create({
   baseURL: '/api/v1',
 })
-Vue.use(VueAxios, client)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).use(VueAxios, axiosClient).mount('#app')
